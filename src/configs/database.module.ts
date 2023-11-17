e0e0e2c98b692244fe95e3ebe7af86d3da4b78c3
Lee SH <snghyun331@gmail.com>
2023-11-17 20:32:56 +0900

    Refactor: 주석 제거

--- src/configs/database.module.ts
+++ src/configs/database.module.ts
@@ -1,23 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: process.env.DB_HOST,
-//       port: parseInt(process.env.DB_PORT, 10), // 10진수
-//       username: process.env.DB_USER,
-//       password: process.env.DB_PW,
-//       database: process.env.DB_NAME,
-//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//       synchronize: false,
-//     }),
-//   ],
-//   exports: [TypeOrmModule],
-// })
-// export class DatabaseModule {}
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
