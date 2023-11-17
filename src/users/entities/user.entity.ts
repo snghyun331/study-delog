ed384fd891cf67d4f19a0a76ef01e55a9a16d5e7
Lee SH <snghyun331@gmail.com>
2023-11-17 16:41:08 +0900

    Feat: create user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('User')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', nullable: false }) // 닉네임을 가입 아이디로
+  nickname: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', nullable: false }) // default 사진 나중에 추가
+  profileImg: string;
+
+  @Column({ type: 'boolean', default: true, nullable: false })
+  isBright: boolean;
+
+  @Column({ type: 'boolean', default: false, nullable: false })
+  isGoogleLogin: boolean;
+
+  @Column({ type: 'boolean', default: false, nullable: false })
+  isOut: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
