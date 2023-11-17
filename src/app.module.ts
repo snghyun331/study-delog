d78f6bcbce226120132edb2f127b2c00a2242585
Lee SH <snghyun331@gmail.com>
2023-11-17 15:16:07 +0900

    Chore: typeorm 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,6 +9,16 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       envFilePath: `src/configs/env/.${process.env.NODE_ENV}.env`,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10), // 10진수
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
     }),
   ],
   controllers: [AppController],
