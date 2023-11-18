ae02b51d3c3e592dc4dd1f2ebd11f6ee387a3014
Lee SH <snghyun331@gmail.com>
2023-11-18 23:30:24 +0900

    Chore: swagger 설치

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { SERVER_PORT, SERVER_PORT_2 } from './utils/constant';
 
@@ -6,6 +7,19 @@ const port = SERVER_PORT || SERVER_PORT_2;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // run swagger
+  const config = new DocumentBuilder()
+    .setTitle('Delog')
+    .setDescription('Delog API description')
+    .setVersion('1.0')
+    .addTag('swagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // run server
   try {
     await app.listen(port);
     console.log(`Server is listening on port ${port} successfully`);
