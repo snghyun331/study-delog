a23e94311367cfe9dd48bfc659ec2d843fb859e4
Lee SH <snghyun331@gmail.com>
2023-11-26 12:33:58 +0900

    Feat: 회원가입 트랜잭션 적용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,23 +7,28 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
+    private dataSource: DataSource,
   ) {}
   async createUser(nickname: string, password: string, profileImg: string) {
     try {
       const hashedPassword = await bcrypt.hash(password, SALT_ROUND);
 
-      const result = await this.saveUser(nickname, hashedPassword, profileImg);
+      const result = await this.saveUserUsingQueryRunner(
+        nickname,
+        hashedPassword,
+        profileImg,
+      );
       return result;
-    } catch (error) {
-      console.error(error);
-      if (error.code === '23505') {
+    } catch (e) {
+      console.error(e);
+      if (e.code === '23505') {
         throw new ConflictException(
           '이 닉네임은 이미 존재합니다. 다른 닉네임을 입력해주세요',
         );
@@ -33,16 +38,29 @@ export class UsersService {
     }
   }
 
-  private async saveUser(
+  private async saveUserUsingQueryRunner(
     nickname: string,
     password: string,
     profileImg: string,
   ) {
-    const user = new UserEntity();
-    user.nickname = nickname;
-    user.password = password;
-    user.profileImg = profileImg;
-    const result = await this.usersRepository.save(user);
-    return result;
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const user = new UserEntity();
+      user.nickname = nickname;
+      user.password = password;
+      user.profileImg = profileImg;
+      const result = await queryRunner.manager.save(user);
+      // throw new InternalServerErrorException();  // 트랜젹션 확인을 위해 일부러 에러를 발생시킴
+      await queryRunner.commitTransaction();
+      return result;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
