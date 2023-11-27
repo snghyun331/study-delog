bafe8a10434b10f160a0e176ad341cc0636a171d
Lee SH <snghyun331@gmail.com>
2023-11-27 23:22:29 +0900

    Feat: 인가 유저 정보를 가져오는 커스텀 데코레이터 구현

---
+++ src/utils/decorators/authuser.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserEntity } from 'src/users/entities/user.entity';
+
+export const AuthUser = createParamDecorator((data, ctx: ExecutionContext): UserEntity => {
+  const req = ctx.switchToHttp().getRequest();
+  return req.user;
+});
