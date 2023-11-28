2d0ad81ca5995465cc7e4d4947b7ee85a7d82d43
Lee SH <snghyun331@gmail.com>
2023-11-28 23:55:14 +0900

    Update: process.env 전부 configservice로 변경

--- src/users/jwt.strategy.ts
+++ src/users/jwt.strategy.ts
@@ -1,11 +1,14 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-custom';
 import * as jwt from 'jsonwebtoken';
-import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super();
+  }
   async validate(req) {
     try {
       const userToken = req.headers['authorization']?.slice(7);
@@ -14,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
         throw new BadRequestException('There is no access token in header');
       }
 
-      const secretKey = process.env.JWT_SECRET_KEY;
+      const secretKey = this.configService.get<string>('JWT_SECRET_KEY');
       jwt.verify(userToken, secretKey);
       const payload = jwt.decode(userToken);
       const userId = payload['userId'];
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,16 +6,21 @@ import { UserEntity } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: {
-        expiresIn: 3600,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [UsersController],
