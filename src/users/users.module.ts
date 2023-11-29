5930ba7cc83097244a345fecb9386aaf2ccf6722
Lee SH <snghyun331@gmail.com>
2023-11-30 00:03:03 +0900

    Refact: 주석 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,25 +7,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.access.strategy';
 import { JwtRefreshStrategy } from './jwt.refresh.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserEntity]),
-    PassportModule,
-    JwtModule.register({}),
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configService: ConfigService) => ({
-    //     secret: configService.get<string>('JWT_SECRET_KEY'),
-    //     signOptions: {
-    //       expiresIn: configService.get<number>('JWT_ACCESS_EXPIRATION_TIME'),
-    //       // expiresIn: 3600,
-    //     },
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity]), PassportModule, JwtModule.register({})],
   controllers: [UsersController],
   providers: [UsersService, JwtStrategy, JwtRefreshStrategy],
   exports: [JwtStrategy, JwtRefreshStrategy, PassportModule],
